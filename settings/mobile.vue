d512b8b199f4f869be07f2d976120daa24ed403a
aoba <113357954+aoaoaobaba@users.noreply.github.com>
2025-03-19 16:48:01 +0900

    mobile.vue

---
+++ settings/mobile.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button @click="callAjax">サーバー呼び出し</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    methods: {
+        callAjax() {
+            fetch('/api/data')
+                .then(response => response.json())
+                .then(data => alert('データ: ' + JSON.stringify(data)))
+                .catch(error => console.error('エラー:', error));
+        }
+    }
+});
+</script>
